e895b0ff42348b1bd4780fbf51be499fa806ecee
Rory Parle <rory@soylentred.net>
2011-03-23 23:32:41 +0000

    Add Swedish translation

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd chrome
-zip makelink.jar $(find content skin locale -type f -or -type d | grep -v '^\.')
+zip makelink.jar $(find content skin locale -type f -or -type d | grep -v '\(/\|^\)\.')
 popd
 zip makelink.xpi chrome/ chrome/makelink.jar chrome.manifest install.rdf defaults/ defaults/preferences/defaults.js
 
